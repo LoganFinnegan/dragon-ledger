9f52a819c96858c6a21c6a6a9597398ec101bf2b
Logan Finnegan <logantfinnegan@protonmail.com>
2025-12-13 22:56:33 -0600

    Add model validations and idempotency specs

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,5 +1,16 @@
-require 'rails_helper'
+require "rails_helper"
 
 RSpec.describe Item, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "requires name, game, and external_id" do
+    item = Item.new
+    expect(item).not_to be_valid
+    expect(item.errors.full_messages.join).to match(/Name|Game|External/i)
+  end
+
+  it "enforces uniqueness of external_id scoped to game" do
+    Item.create!(name: "Steadfast boots", game: "rs3", external_id: 21787)
+
+    dup = Item.new(name: "Steadfast boots", game: "rs3", external_id: 21787)
+    expect(dup).not_to be_valid
+  end
 end
