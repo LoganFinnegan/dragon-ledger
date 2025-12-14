ecde17673048b6f4f5d10c9c97d995449112f5c9
Logan Finnegan <logantfinnegan@protonmail.com>
2025-12-14 16:37:21 -0600

    Add items UI with Chart.js price chart

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root "pages#home"
-  get "/items", to: "pages#items"
+
+  resources :items, only: %i[index show]
 
   namespace :api do
     resources :items, only: %i[index show] do
